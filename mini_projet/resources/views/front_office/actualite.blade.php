e661ffdfde3847dec9b39cc5bc9898b18464506a
TiavinaRavaka <99461072+Tiavina23@users.noreply.github.com>
2023-05-08 13:01:27 +0300

    Update actualite.blade.php

--- mini_projet/resources/views/front_office/actualite.blade.php
+++ mini_projet/resources/views/front_office/actualite.blade.php
@@ -4,6 +4,11 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="description" content="Découvrez les dernières actualités et tendances de l'intelligence artificielle . Restez informé des derniers développements en matière d'IA dans différents domaines.">
+  <meta property="og:title" content="Actualités de l'intelligence artificielle | Nom de la Société">
+  <meta property="og:description" content="Découvrez les dernières actualités et tendances de l'intelligence artificielle . Restez informé des derniers développements d'IA dans différents domaines.">
+  <meta property="og:image" content="https://exemple.com/images/ia.jpg">
+  <meta name="keywords" content="Intelligence artificielle, IA, actualités, tendances, santé, industrie, commerce ,evolution">
 
   <title>Actualités de l'intelligence artificielle </title>
 
@@ -19,9 +24,6 @@
   <link href="{{secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
   <link href="{{secure_asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
   <link href="{{secure_asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
@@ -191,9 +193,6 @@
   <!-- Vendor JS Files -->
   <script src="{{ secure_asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/quill/quill.min.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/php-email-form/validate.js')}}"></script>
