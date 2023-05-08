7c37aa59099e5dd976def4a1a4d9ef235df3bce8
TiavinaRavaka <99461072+Tiavina23@users.noreply.github.com>
2023-05-08 14:19:47 +0300

    Update actualite.blade.php

--- mini_projet/resources/views/front_office/actualite.blade.php
+++ mini_projet/resources/views/front_office/actualite.blade.php
@@ -36,10 +36,10 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+    
         <img src="{{ secure_asset('assets/img/logo.png')}}" alt="">
         <span class="d-none d-lg-block">INTEL_ARTIF</span>
-      </a>
+    
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
