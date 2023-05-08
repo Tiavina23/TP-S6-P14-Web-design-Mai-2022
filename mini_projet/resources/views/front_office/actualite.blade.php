3a1a6b30c6a65fa2a761f7185779e92cd7bdb6d3
TiavinaRavaka <99461072+Tiavina23@users.noreply.github.com>
2023-05-08 19:11:31 +0300

    Update actualite.blade.php

--- mini_projet/resources/views/front_office/actualite.blade.php
+++ mini_projet/resources/views/front_office/actualite.blade.php
@@ -129,13 +129,13 @@
 
 
                   <div class="carousel-item active">
-                    <img src="/uploads/base64,{{$tab->image}}" class="d-block w-100" alt="...">
+                    <img src="/uploads/{{$tab->image}}" class="d-block w-100" alt="...">
                   </div>
                   <div class="carousel-item">
-                    <img src="/uploads/base64,{{$tab->image}}" class="d-block w-100" alt="...">
+                    <img src="/uploads/{{$tab->image}}" class="d-block w-100" alt="...">
                   </div>
                   <div class="carousel-item">
-                    <img src="/uploads/base64,{{$tab->image}}" class="d-block w-100" alt="...">
+                    <img src="/uploads/{{$tab->image}}" class="d-block w-100" alt="...">
                   </div>
                 </div>
 
