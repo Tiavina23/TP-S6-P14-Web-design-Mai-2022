2fd8e7a09d36e98cb359cf36128f903da62b4f0f
TiavinaRavaka <99461072+Tiavina23@users.noreply.github.com>
2023-05-08 14:22:39 +0300

    Update fiche.blade.php

--- mini_projet/resources/views/front_office/fiche.blade.php
+++ mini_projet/resources/views/front_office/fiche.blade.php
@@ -18,11 +18,6 @@
 
   <!-- Vendor CSS Files -->
   <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
   <link href="{{secure_asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
@@ -250,18 +245,9 @@
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="{{ secure_asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
   <script src="{{ secure_asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/quill/quill.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ secure_asset('assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{secure_asset('assets/js/main.js')}}"></script>
 
-  <!-- Template Main JS File -->
-  <script src="{{ secure_asset('assets/js/main.js')}}"></script>
 
 </body>
 
